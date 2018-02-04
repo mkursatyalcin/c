89a46fc4679f6c7cdd1f7c312f1fd0fa20b918f9
mkursatyalcin <36080145+mkursatyalcin@users.noreply.github.com>
2018-02-04 20:12:17 +0300

    Create hafta4_ornek4.c

---
+++ hafta4_ornek4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int sayi1 = 10;
+    int sayi2 = 20;
+    int sonuc = 0;
+
+    sonuc = sayi1 + sayi2;
+    printf("Toplam = %d \n", sonuc);
+
+    return 0;
+}
