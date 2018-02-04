4d4f5858520eccf31d9b22bc669dad9375b1110b
mkursatyalcin <36080145+mkursatyalcin@users.noreply.github.com>
2018-02-04 16:52:36 +0300

    Create hafta4_ornek3.c

---
+++ hafta4_ornek3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char karakter = 'R';
+    float sayi = 7.2;
+    int tamsayi = 65;
+
+    printf("Harf = %c \n", karakter);
+    printf("Ondalikli sayi = %.2f \n", sayi);
+    printf("Tam sayi = %d \n", tamsayi);
+
+    return 0;
+}
