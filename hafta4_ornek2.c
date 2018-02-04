945feefaede0c1152e77f823fb3ae05324b1ba2f
mkursatyalcin <36080145+mkursatyalcin@users.noreply.github.com>
2018-02-04 16:43:45 +0300

    Create hafta4_ornek2.c

---
+++ hafta4_ornek2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int sayi1 = 12;
+    int sayi2 = 7;
+
+    printf("birinci sayi = %d \n", sayi1);
+    printf("ikinci sayi = %d \n", sayi2);
+
+    return 0;
+}
