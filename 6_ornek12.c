b2d432d736274ef8b5dd489292431db3bd626f35
mkursatyalcin <36080145+mkursatyalcin@users.noreply.github.com>
2018-03-06 06:45:05 +0300

    Update 6_ornek12.c

--- 6_ornek12.c
+++ 6_ornek12.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main()
+{
+    int sayi,toplam=0,i=1;
+    char kontrol;
+
+    while(1)
+    {
+        printf("%d. sayiyi giriniz:",i);
+        scanf("%d",&sayi);
+        toplam+=sayi;
+
+        printf("Yeni sayi girmek istiyor musun? (E/H) : ");
+        scanf(" %c",&kontrol);
+
+        if(kontrol=='H')
+        {
+            break;
+        }
+        i++;
+    }
+
+    printf("%d sayi girdiniz.\n",i);
+    printf("Girilen sayilarin toplami = %d \n",toplam);
+
+    return 0;
+}
