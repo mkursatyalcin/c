8e0b415f19bbcd14dbd2606d6789edc24db223dc
mkursatyalcin <36080145+mkursatyalcin@users.noreply.github.com>
2018-03-19 22:35:47 +0300

    Create 7_ornek2.c

---
+++ 7_ornek2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("%.1f \n", sqrt(9));
+    printf("%.1f \n", pow(2,3));
+    printf("%.1f \n",log(1));
+    printf("%.30f \n", M_PI);
+    return 0;
+}
